c9c27de4a3b909ab42e2b36d6cd08797478a1f6f
Ilia Formalsky <formal777@gmail.com>
2011-06-16 18:54:35 +0400

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,13 @@ class User < ActiveRecord::Base
     encrypted_password==encrypt(submitted_password)
   end
 
-  def self.authenticate(email, submitted_password)
-     user=find_by_email(email)
-     return nil  if user.nil?
-     return user if user.has_password?(submitted_password) 
+ def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
   end
-   
+
+
 
    private
  
